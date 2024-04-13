3790be4b642bcca41705bda9f40d923e9fadc056
haron murumba <haron.murumba22@gmail.com>
2024-04-13 11:00:04 +0300

    for geeks to

---
+++ recursive.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+houses = ["erick's house", "haron'shouse", "ann's house", "hannas's house", ]
+def deliver_present_to_houses():
+    for house in houses:
+        print("i delivered the present to", house)
+        
+deliver_present_to_houses()
