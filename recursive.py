59f4ea85ac1ed970905560b16e652320f47794c9
haron murumba <haron.murumba22@gmail.com>
2024-04-13 11:21:07 +0300

    divsion of labour

--- recursive.py
+++ recursive.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 
 houses = ["erick's house", "haron'shouse", "ann's house", "hannas's house", ]
-def deliver_present_to_houses():
+def deliver_present_to_houses(houses):
     for house in houses:
         print("i delivered the present to", house)
+        if len(houses) == 1:
+            house = houses[0]
+    else:
+        mid = len(house) //2
+        first_half = houses[:mid]
+        second_half = houses[mid:]
         
-deliver_present_to_houses()
+deliver_present_to_houses(first_half)
+deliver_present_to_houses(second_half)
